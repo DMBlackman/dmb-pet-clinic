fbe0a456d7fa9ea6b0bc25ca74a35f5421798274
Declan <declan.blackman1@gmail.com>
2021-06-30 10:16:59 -0600

    Initial Project Add

---
+++ src/main/java/dmb/springframework/dmbpetclinic/DmbPetClinicApplication.java
@@ -0,0 +1,13 @@
+package dmb.springframework.dmbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmbPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmbPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dmb/springframework/dmbpetclinic/DmbPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package dmb.springframework.dmbpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmbPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
